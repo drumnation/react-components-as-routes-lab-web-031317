a16bb2925957942708bba57f744e45f8838cafe1
drumnation <davidmieloch@gmail.com>
2017-05-21 19:13:38 -0400

    works in browser but can't find app.js and fails tests

--- src/Routes.js
+++ src/Routes.js
@@ -10,4 +10,12 @@ import Movies from './components/Movies';
 
 export default (
   /* add routes here */
-);
+  <Router history={browserHistory}>
+    <Route path="/" component={App} >
+      <IndexRoute component={Home} />
+      <Route path="/movies" component={Movies} />
+      <Route path="/actors" component={Actors} />
+      <Route path="/directors" component={Directors} />
+    </Route>
+  </Router>
+)
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,15 @@
 import React from 'react';
-import { Link } from 'react-router';
+import { NavLink } from 'react-router-dom';
 
 const NavBar = () => {
-  return (<div />);
+  return (
+    <div className="navbar">
+      <NavLink to="/">Home</NavLink>
+      <NavLink to="/movies">Movies</NavLink>
+      <NavLink to="/directors">Directors</NavLink>
+      <NavLink to="/actors">Actors</NavLink>
+    </div>
+  );
 };
 
 export default NavBar;
